e31133d53793bba73f51a95fc8a3d95300380976
sapienhwaker <sapienhwaker@gmail.com>
2021-08-20 03:14:04 -0400

    added comment to index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res)=>{
     res.send("<h2>I am India</h2>");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //either pick the port mentioned in the environment variables or 3000'''
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
